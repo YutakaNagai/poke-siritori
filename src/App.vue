f82b0c096ce775bc72c56728585debaf8b044695
YutakaNagai <ynagai103@gmail.com>
2023-09-04 01:46:26 +0900

    入力地のバリデーション

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,40 @@ import { ref } from "vue";
 const inputMsg = ref("");
 const history = ref([]);
 const loseMsgList = ref([]);
+const errorMsgList = ref([]);
 
 const submitMsg = () => {
+  // エラーメッセージの初期化
+  errorMsgList.value = [];
+
   // チェック処理
-  // 「ん」で終わっていたら敗北
-  const lastChar = inputMsg.value.slice(-1);
-  if (lastChar === "ん" || lastChar === "ン") {
-    loseMsgList.value.push(`「${lastChar}」 で終わってしまった`);
-  }
-
-  // 同じ単語が出てきたら敗北
-  if (history.value.includes(inputMsg.value)) {
-    loseMsgList.value.push(`「${inputMsg.value}」 は既に使用済み`);
-  }
-
-  // 前の単語と繋がっていなかったら敗北
-  if (history.value.length > 0) {
-    const lastWordLastChar = history.value.slice(-1)[0].substr(-1);
-    const currentWordFirstChar = inputMsg.value[0];
-    if (kanaToHira(lastWordLastChar) !== kanaToHira(currentWordFirstChar)) {
-      loseMsgList.value.push(`前回の単語の末尾は 「${lastWordLastChar}」`);
+  if (validateMsg()) {
+    // 敗北判定
+    // 「ん」で終わっていたら敗北
+    const lastChar = inputMsg.value.slice(-1);
+    if (lastChar === "ん" || lastChar === "ン") {
+      loseMsgList.value.push(`「${lastChar}」 で終わってしまった`);
     }
-  }
 
-  // 問題なければ履歴にpush
-  history.value.push(inputMsg.value);
-  // テキストボックスの初期化
-  inputMsg.value = "";
+    // 同じ単語が出てきたら敗北
+    if (history.value.includes(inputMsg.value)) {
+      loseMsgList.value.push(`「${inputMsg.value}」 は既に使用済み`);
+    }
+
+    // 前の単語と繋がっていなかったら敗北
+    if (history.value.length > 0) {
+      const lastWordLastChar = history.value.slice(-1)[0].substr(-1);
+      const currentWordFirstChar = inputMsg.value[0];
+      if (kanaToHira(lastWordLastChar) !== kanaToHira(currentWordFirstChar)) {
+        loseMsgList.value.push(`前回の単語の末尾は 「${lastWordLastChar}」`);
+      }
+    }
+
+    // 問題なければ履歴にpush
+    history.value.push(inputMsg.value);
+    // テキストボックスの初期化
+    inputMsg.value = "";
+  }
 };
 
 const retryGame = () => {
@@ -45,6 +52,20 @@ const kanaToHira = (str) => {
     return String.fromCharCode(chr);
   });
 };
+
+const validateMsg = () => {
+  if (!inputMsg.value) {
+    errorMsgList.value.push("ワードを入力してください");
+    return false;
+  } else if (!inputMsg.value.match(/^[ぁ-んァ-ヶー]*$/)) {
+    errorMsgList.value.push(
+      "全角のひらがな・カタカナ・長音記号のみ入力可能です"
+    );
+    return false;
+  } else {
+    return true;
+  }
+};
 </script>
 
 <template>
@@ -55,8 +76,17 @@ const kanaToHira = (str) => {
       <p v-if="history.length > 0">
         前回捕まえたポケモン: {{ history.slice(-1)[0] }}
       </p>
-      <input v-model="inputMsg" />
+      <input
+        type="text"
+        v-model="inputMsg"
+        required
+        pattern="[\u30A1-\u30FC]*"
+      />
       <button @click="submitMsg">送信</button>
+
+      <p v-for="(msg, index) in errorMsgList" :key="index" class="error_msg">
+        {{ msg }}
+      </p>
     </template>
 
     <!-- 敗北時処理 -->
@@ -79,5 +109,9 @@ const kanaToHira = (str) => {
 <style scoped>
 input {
   font-size: 16px;
+}
+
+.error_msg {
+  color: red;
 }
 </style>
