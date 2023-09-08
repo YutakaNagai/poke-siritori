2642ce2fe12259de81ab73a219ad2d8535a2f967
YutakaNagai <ynagai103@gmail.com>
2023-09-08 19:49:58 +0900

    json追加

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Vue 3 + Vite
+# ポケしりとり
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+## 開発環境URL
 
-## Recommended IDE Setup
+`https://poke-siritori.pages.dev/`
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+## ツール
+
+### スクレイピングとjson作成
+
+ルートディレクトリで下記を実行
+`$ python src/tools/scrapingTable.py; node src/tools/formatCsv.js;`
--- src/tools/formatCsv.js
+++ src/tools/formatCsv.js
@@ -2,24 +2,41 @@ import csv from "csvtojson";
 import fs from "fs";
 import log4js from "log4js";
 
-const targetFileName = "poke";
-const csvFilePath = `src/data/csv/${targetFileName}.csv`;
-const jsonFilePath = `src/data/json/${targetFileName}.json`;
-
 const logger = log4js.getLogger();
 logger.level = "all";
 
-csv()
-  .fromFile(csvFilePath)
-  .then((rows) => {
-    rows = rows.map((row) => {
-      return row;
+const targetFileNameList = [
+  "poke",
+  "tokusei",
+  "waza_1",
+  "waza_2",
+  "waza_3",
+  "waza_4",
+  "waza_5",
+  "waza_6",
+  "waza_7",
+  "waza_8",
+  "waza_9",
+  "waza_pae",
+  "waza_pla",
+];
+
+for (const targetFileName of targetFileNameList) {
+  const csvFilePath = `src/data/csv/${targetFileName}.csv`;
+  const jsonFilePath = `src/data/json/${targetFileName}.json`;
+
+  csv()
+    .fromFile(csvFilePath)
+    .then((rows) => {
+      rows = rows.map((row) => {
+        return row;
+      });
+      fs.writeFile(jsonFilePath, JSON.stringify(rows, null, 2), (err) => {
+        if (err) {
+          logger.warn(`An error occured in generated ${jsonFilePath}.`);
+          throw err;
+        }
+        logger.info("JSON generated.");
+      });
     });
-    fs.writeFile(jsonFilePath, JSON.stringify(rows, null, 2), (err) => {
-      if (err) {
-        logger.warn(`An error occured in generated ${jsonFilePath}.`);
-        throw err;
-      }
-      logger.info("JSON generated.");
-    });
-  });
+}
